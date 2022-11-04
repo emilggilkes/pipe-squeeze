2c20bb15cffab2bdc13f30c5d0300776027e801b
Ubuntu <ubuntu@ip-172-31-84-69.ec2.internal>
2022-11-04 00:50:31 +0000

    DDP

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,14 +2,29 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import torch.distributed as dist
 from torch import nn, optim
 from torchvision import datasets, transforms, models
 from torchvision.datasets import ImageFolder, ImageNet
 from torch.utils.data import DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+import torch.multiprocessing as mp
 
 
 
-def create_data_loader(batch_size = 32, num_workers = 4):
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    dist.init_process_group(backend="nccl", rank = rank, world_size = world_size)
+    print(f"Initiated Process Group with rank = {rank} and world_size = {world_size}")
+
+def cleanup():
+    dist.destroy_process_group()
+
+def create_data_loader(rank, world_size, batch_size = 32, num_workers = 0):
+    print(f"Create data loader device {rank}")
     train_transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.ToTensor(),
@@ -18,37 +33,48 @@ def create_data_loader(batch_size = 32, num_workers = 4):
         transforms.RandomResizedCrop(224),
         transforms.ToTensor()
     ])
-    train_set = ImageFolder("../data/images/train", transform = train_transform)
-    val_set   = ImageFolder("../data/images/val", transform = val_transform)
+    train_set = ImageFolder("../data/ImageNet/train", transform = train_transform)
+    val_set   = ImageFolder("../data/ImageNet/val", transform = val_transform)
 
+    train_sampler = DistributedSampler(train_set, num_replicas=world_size, rank=rank, shuffle=True, drop_last=False)
+    val_sampler = DistributedSampler(val_set, num_replicas=world_size, rank=rank, shuffle=True, drop_last=False)
 
     train_loader = DataLoader(
         dataset=train_set,
         batch_size = batch_size,
         num_workers= num_workers,
-        shuffle=True,
-    )
+	sampler=train_sampler,
+)
 
     val_loader = DataLoader(
         dataset=val_set,
         batch_size = batch_size,
         num_workers = num_workers,
-        shuffle=True,
+	sampler=val_sampler,
     )
     return train_loader, val_loader
 
 
-def train(model, train_loader, val_loader, epochs = 1, plot = True):
-    print("Start Training...")
-    criterion = nn.CrossEntropyLoss().to(device)
+def train(rank, world_size, epochs = 3):
+    setup(rank, world_size)
+
+    vgg19 = models.vgg19(weights = None)
+    vgg19.to(rank)
+    vgg19 = DDP(vgg19, device_ids=[rank], output_device=rank)
+
+    train_loader, val_loader = create_data_loader(rank, world_size)
+
+    criterion = nn.CrossEntropyLoss().to(rank)
     optimizer = optim.SGD(vgg19.parameters(), lr = 0.003, momentum=0.9, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
+    print(f"Start Training device {rank}")
     running_loss = 0
     train_losses, val_losses = [], []
     for epoch in range(epochs):
+        train_loader.sampler.set_epoch(epoch)
         for inputs, labels in train_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
+            inputs, labels = inputs.to(rank), labels.to(rank)
             optimizer.zero_grad()
             logps = vgg19.forward(inputs)
             loss = criterion(logps, labels)
@@ -59,13 +85,13 @@ def train(model, train_loader, val_loader, epochs = 1, plot = True):
             labels.detach()
             logps.detach()
 
-
         val_loss = 0
         accuracy = 0
         vgg19.eval()
         with torch.no_grad():
+            val_loader.sampler.set_epoch(epoch)
             for inputs, labels in val_loader:
-                inputs, labels = inputs.to(device), labels.to(device)
+                inputs, labels = inputs.to(rank), labels.to(rank)
                 logps = vgg19.forward(inputs)
                 batch_loss = criterion(logps, labels)
                 val_loss += batch_loss.item()
@@ -79,33 +105,33 @@ def train(model, train_loader, val_loader, epochs = 1, plot = True):
         train_losses.append(running_loss/len(train_loader))
         val_losses.append(val_loss/len(val_loader))
         print(f"Epoch {epoch+1}/{epochs}   "
+		f"Device {rank}   "
                 f"Train loss: {running_loss/len(train_loader):.3f}   "
                 f"Validation loss: {val_loss/len(val_loader):.3f}   "
                 f"Validation accuracy: {accuracy/len(val_loader):.3f}")
         running_loss = 0
         vgg19.train()
-    torch.save(vgg19, '../../models/vgg19.pth')
-    print("Finished Training")
-    if plot:
+    cleanup()
+    
+    torch.save(vgg19.state_dict(), f'../../models/vgg19_{rank}.pth')
+    
+    print(f"Finished Training device {rank}")
+    if False:
         plt.plot(train_losses, label='Training loss')
         plt.plot(val_losses, label='Validation loss')
         plt.legend(frameon=False)
         plt.show()
 
+
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    ##MODEL
-    vgg19 = models.vgg19(weights = None)
-    vgg19.to(device)
-
-    ##DATASET
-    train_loader, val_loader = create_data_loader(batch_size = 4, num_workers = 2)
-
-    ##TRAINING
-    train(vgg19, train_loader, val_loader)
-
-
-
+    n = min(torch.cuda.device_count(), 4)
+    print(f'Using device {device} with device count : {n}')
+    world_size = n
+    mp.spawn(
+        train,
+        args=(world_size,),
+        nprocs=world_size
+    )
 
 #https://github.com/pytorch/examples/blob/main/imagenet/main.py
